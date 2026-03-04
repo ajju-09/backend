42d57941403a5cc39ec99b344e0dbe98e0da6dad
Ajju <ajinkyaawachar33@gmail.com>
2026-03-04 12:29:54 +0530

    Create table and model for plans

---
+++ models/v3/plan.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const Plan = sequelize.define(
+    "Plan",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      type: {
+        type: DataTypes.ENUM("Free", "Premium"),
+        allowNull: false,
+        defaultValue: "Free",
+      },
+      price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.0,
+      },
+      duration_days: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      daily_message_limit: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      file_sharing_enable: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "Plans",
+      timestamps: true,
+    },
+  );
+
+  return Plan;
+};
